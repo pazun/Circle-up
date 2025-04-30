d81281deca93c4c6604e653db21f808561fa254c
pazun <silantyevnikitaaa@gmail.com>
2025-04-30 09:13:03 +0500

    Added groups

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, EmailField
+from wtforms import StringField, PasswordField, EmailField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, ValidationError
 import os
 
@@ -12,6 +12,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = 'bekne'
 db = SQLAlchemy(app)
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    admin_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    image = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    
+    def __repr__(self):
+        return f"Group('{self.name}', Created by User ID: {self.admin_id})"
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +32,7 @@ class User(db.Model):
     image_profile = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    groups_admin = db.relationship('Group', backref='admin', lazy=True)
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_profile}')"
@@ -109,19 +121,104 @@ def signup():
         return redirect(url_for('login'))
     return render_template('signup.html', form=form)
 
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 def account():
     if 'user_id' not in session:
         return redirect(url_for('login'))
     
+    form = UpdateAccountForm()
     user = User.query.get(session['user_id'])
+    
+    if form.validate_on_submit():
+        if form.image_profile.data:
+            file = form.image_profile.data
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            user.image_profile = filename
+            
+        if form.location.data:
+            user.location = form.location.data
+            
+        db.session.commit()
+        return redirect(url_for('account'))
+        
     if user:
-        return render_template('account.html', user=user)
+        return render_template('account.html', user=user, form=form)
     return redirect(url_for('login'))
+
+@app.route('/account', methods=['GET', 'POST'])
+class UpdateAccountForm(FlaskForm):
+    location = StringField('Location')
+    image_profile = FileField('Profile Picture', validators=[
+        FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Images only!')
+    ])
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists. Please choose a different one.')
+            
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already registered. Please use a different email.')
+
+@app.route('/users')
+def users():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
+    users = User.query.all()
+    current_user = User.query.get(session['user_id'])
+    return render_template('users.html', users=users, current_user=current_user)
+
+class GroupForm(FlaskForm):
+    name = StringField('Group Name', validators=[
+        DataRequired(),
+        Length(min=3, max=100, message='Group name must be between 3 and 100 characters')
+    ])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    image = FileField('Group Image', validators=[
+        FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Images only!')
+    ])
+
+@app.route('/groups')
+def groups():
+    groups = Group.query.all()
+    form = GroupForm()
+    return render_template('groups.html', groups=groups, form=form)
+
+@app.route('/create_group', methods=['POST'])
+def create_group():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+        
+    form = GroupForm()
+    if form.validate_on_submit():
+        image_file = 'default.jpg'
+        if form.image.data:
+            file = form.image.data
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER_GROUPS'], filename))
+            image_file = filename
+            
+        group = Group(
+            name=form.name.data,
+            description=form.description.data,
+            image=image_file,
+            admin_id=session['user_id']
+        )
+        db.session.add(group)
+        db.session.commit()
+        return redirect(url_for('groups'))
+    return redirect(url_for('groups'))
 
 UPLOAD_FOLDER = 'static/user_images'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+UPLOAD_FOLDER_GROUPS = 'static/group_images'
+app.config['UPLOAD_FOLDER_GROUPS'] = UPLOAD_FOLDER_GROUPS
 
 if __name__ == '__main__':
     app.run()
